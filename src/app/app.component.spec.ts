26c14035005217a99a5960f49bd52e03ed1bc29c
Hercules Konsoulas <dyka3773@gmail.com>
2025-11-07 15:55:09 +0100

    chore: change the project's name pt2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'astrobeam' title`, () => {
+  it(`should have the 'web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('astrobeam');
+    expect(app.title).toEqual('web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, astrobeam');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web');
   });
 });
